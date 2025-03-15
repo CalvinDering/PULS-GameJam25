b830aa4c52ef23e32abd7ec4b295a40365f22123
CalvinDering <calvin.dering@gmail.com>
2025-03-15 01:43:59 +0100

    Changed deathplane to play animation till finished

--- PULS-GameJam25/Assets/_Scripts/Deathplane.cs
+++ PULS-GameJam25/Assets/_Scripts/Deathplane.cs
@@ -3,7 +3,25 @@ using UnityEngine;
 public class Deathplane : MonoBehaviour {
     private void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Rocket")) {
-            Destroy(other.transform.parent.gameObject);
+            GameObject rocket = other.transform.parent.gameObject;
+            ParticleSystem[] ps = rocket.GetComponentsInChildren<ParticleSystem>();
+
+            if(ps.Length > 0) {
+                rocket.GetComponentInChildren<Collider>().enabled = false;
+                rocket.GetComponentInChildren<MeshRenderer>().enabled = false;
+
+                float maxLifeTime = 0f;
+
+                foreach(ParticleSystem particleSystem in ps) {
+                    particleSystem.Stop();
+                    float lifeTime = particleSystem.main.duration + particleSystem.main.startLifetime.constantMax;
+                    if(lifeTime > maxLifeTime) {
+                        maxLifeTime = lifeTime;
+                    }
+                }
+
+                Destroy(rocket, maxLifeTime);
+            }
         }
     }
 }
