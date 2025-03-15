aa7b296fdb2e5610d4fe9c93a20390440f853431
CalvinDering <calvin.dering@gmail.com>
2025-03-15 23:04:13 +0100

    Added more road prefabs

--- PULS-GameJam25/Assets/_Scripts/HouseSpawner.cs
+++ PULS-GameJam25/Assets/_Scripts/HouseSpawner.cs
@@ -13,6 +13,7 @@ public class HouseSpawner : MonoBehaviour {
     private int cellsize;
 
     private void Awake() {
+        cellsize = houseSize / 5;
 
         int spawnIndex = Random.Range(0, housePrefabs.Length);
         for(int x = 0; x < width; x++) {
@@ -25,29 +26,39 @@ public class HouseSpawner : MonoBehaviour {
 
                 Instantiate(housePrefabs[spawnIndex], spawnPosition, spawnRotation, transform);
 
-                cellsize = houseSize / 5;
-
-                if(y != width - 1) {
-                    for(int i = 0; i < cellsize; i++) {
-                        Vector3 roadSpawnPosition1 = new Vector3((i + 1) * cellsize + x * houseSize + x * roadOffset, 0, (y + 1) * houseSize + cellsize + y * roadOffset);
-                        Vector3 roadSpawnPosition2 = new Vector3((i + 1) * cellsize + x * houseSize + x * roadOffset, 0, (y + 1) * houseSize + 2 * cellsize + y * roadOffset);
-
-                        Instantiate(roadPrefabs[spawnIndex], roadSpawnPosition1, Quaternion.identity, transform);
-                        Instantiate(roadPrefabs[spawnIndex], roadSpawnPosition2, Quaternion.identity, transform);
-                    }
-                }
+                SpawnRoadAfterHouse(x, y);
             }
 
-            if(x != width - 1) {
-                for(int i = 0; i < width * houseSize / cellsize + (width - 1) * 2; i++) {
-                    Vector3 roadSpawnPosition1 = new Vector3((x + 1) * houseSize + cellsize + x * roadOffset, 0, (i + 1) * cellsize);
-                    Vector3 roadSpawnPosition2 = new Vector3((x + 1) * houseSize + 2 * cellsize + x * roadOffset, 0, (i + 1) * cellsize);
-
-                    Instantiate(roadPrefabs[spawnIndex], roadSpawnPosition1, Quaternion.identity, transform);
-                    Instantiate(roadPrefabs[spawnIndex], roadSpawnPosition2, Quaternion.identity, transform);
-                }
-            }
-
+            SpawnRoadRow(x);
         }
+    }
+
+    private void SpawnRoadAfterHouse(int x, int y) {
+        if(y != width - 1) {
+            for(int i = 0; i < cellsize; i++) {
+                Vector3 roadSpawnPosition1 = new Vector3((i + 1) * cellsize + x * houseSize + x * roadOffset, 0, (y + 1) * houseSize + cellsize + y * roadOffset);
+                Vector3 roadSpawnPosition2 = new Vector3((i + 1) * cellsize + x * houseSize + x * roadOffset, 0, (y + 1) * houseSize + 2 * cellsize + y * roadOffset);
+
+                SpawnRoad(roadSpawnPosition1);
+                SpawnRoad(roadSpawnPosition2);
+            }
+        }
+    }
+
+    private void SpawnRoadRow(int row) {
+        if(row != width - 1) {
+            for(int i = 0; i < width * houseSize / cellsize + (width - 1) * 2; i++) {
+                Vector3 roadSpawnPosition1 = new Vector3((row + 1) * houseSize + cellsize + row * roadOffset, 0, (i + 1) * cellsize);
+                Vector3 roadSpawnPosition2 = new Vector3((row + 1) * houseSize + 2 * cellsize + row * roadOffset, 0, (i + 1) * cellsize);
+
+                SpawnRoad(roadSpawnPosition1);
+                SpawnRoad(roadSpawnPosition2);
+            }
+        }
+    }
+
+    private void SpawnRoad(Vector3 spawnPosition) {
+        int randomSpawnIndex = Random.Range(0, roadPrefabs.Length);
+        Instantiate(roadPrefabs[randomSpawnIndex], spawnPosition, Quaternion.identity, transform);
     }
 }
