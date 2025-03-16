385cda422ba11afbde288e87f85ea6dde9cf8e80
CalvinDering <calvin.dering@gmail.com>
2025-03-16 17:00:11 +0100

    Added line renderer

--- PULS-GameJam25/Assets/_Scripts/Player/PlayerShoot.cs
+++ PULS-GameJam25/Assets/_Scripts/Player/PlayerShoot.cs
@@ -9,8 +9,23 @@ public class PlayerShoot : MonoBehaviour {
 
     [SerializeField] private float bulletSpeed = 10f;
 
+    [SerializeField] private LineRenderer lineRenderer;
+    [SerializeField] private int linePoints = 175;
+    [SerializeField] private float timeIntervalInPoints = 0.01f;
+
     private void Awake() {
         weaponSound = GetComponent<AudioSource>();
+        if(lineRenderer != null) {
+            lineRenderer.enabled = true;
+        }
+    }
+
+    private void Update() {
+        if(lineRenderer == null) {
+            return;
+        }
+
+        DrawTrajectory();
     }
 
     public void Shoot() {
@@ -23,5 +38,22 @@ public class PlayerShoot : MonoBehaviour {
         rocketRigidbody.AddForce(rocket.transform.forward * bulletSpeed, ForceMode.Impulse);
 
         weaponSound.Play();
+    }
+
+    private void DrawTrajectory() {
+        Vector3 origion = rocketSpawnpoint.position;
+        Vector3 startVelocity = bulletSpeed * rocketSpawnpoint.forward;
+        lineRenderer.positionCount = linePoints;
+        float time = 0;
+        for(int i = 0; i < linePoints; i++) {
+            float x = (startVelocity.x * time) + (Physics.gravity.x / 2 * time * time);
+            float y = (startVelocity.y * time) + (Physics.gravity.y / 2 * time * time);
+            float z = (startVelocity.z * time) + (Physics.gravity.z / 2 * time * time);
+            Vector3 point = new Vector3(x, y, z);
+            lineRenderer.SetPosition(i, origion + point);
+            time += timeIntervalInPoints;
+        }
+    
+
     }
 }
